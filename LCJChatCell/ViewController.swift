828bcb2f568fdaf4426ee21cc746fdd93475e061
Liao Jiue-Ren <xrickliao@gmail.com>
2017-11-24 13:24:27 +0800

    Initial Commit

---
+++ LCJChatCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LCJChatCell
+//
+//  Created by Liao Jiue-Ren on 24/11/2017.
+//  Copyright © 2017 Liao Jiue-Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
